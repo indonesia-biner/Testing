b5c6caa4fa87b7a9ff59c475f81f4ac26ee59b3b
masuden0000 <hudarasyad456@gmail.com>
2023-07-21 05:15:13 +0700

    perubahan informasi About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -45,8 +45,7 @@ const About = () => {
           >
             <h1 className="h1 font-primary lg:mt-o mt-[220px] lg:mt-[160px]">About me</h1>
             <p className="mb-12 max-w-sm font-primary font-normal w-4/5 md:container">
-              Kenalin nama gw <b> Huda Rasyad Wicaksono </b>, Anak tahun 2000-an awal. Selamat datang di website sederhana gw. Menurut gw, website ini akan berguna untuk sekali untuk gw
-              pribadi dalam menjalani kehidupan di era modernisasi.
+              Kenalin nama gw <b> Huda Rasyad Wicaksono </b>, Anak tahun 2000-an awal. Selamat datang di website sederhana gw. Menurut gw, website ini akan berguna sekali untuk gw dalam menjalani kehidupan di era modernisasi saat ini.
               <br />
               <br />
               Menurut gw, visual #1. Contohnya website saat ini. Kenapa anda semua ingin membaca
